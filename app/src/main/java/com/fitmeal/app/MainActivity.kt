563170c933ace1def79ff79c98bccd49275fcb63
alekszak006 <alekszak06@icloud.com>
2025-05-31 19:05:05 +0200

    Dodano fragment AddProductFragment i integrację z MainActivity
    - Utworzono plik fragment_add_product.xml z formularzem dodawania produktu
    - Stworzono klasę AddProductFragment i podpięto layout
    - Dodano layout activity_main.xml z kontenerem fragment_container
    - Zmieniono MainActivity.kt, aby dynamicznie ładować fragment przy starcie aplikacji
    - Poprawiono motyw aplikacji na Theme.AppCompat (Theme.FitMeal)
    - Naprawiono błąd uruchamiania związany z brakiem motywu AppCompat

--- app/src/main/java/com/fitmeal/app/MainActivity.kt
+++ app/src/main/java/com/fitmeal/app/MainActivity.kt
@@ -1,47 +1,21 @@
 package com.fitmeal.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.fitmeal.app.ui.theme.FitMealTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FitMealTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)  // ustawiamy layout XML
+
+        // Dodajemy fragment do kontenera (jeśli to pierwszy start)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AddProductFragment())
+                .commit()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitMealTheme {
-        Greeting("Android")
-    }
-}
