52f0d1580218afe19ef30de241f793353d5cc880
alekszak006 <alekszak06@icloud.com>
2025-05-31 18:05:17 +0200

    Dodanie bazy danych do projektu

---
+++ app/src/main/java/com/fitmeal/app/database/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.fitmeal.app.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.fitmeal.app.database.dao.ProductDao
+import com.fitmeal.app.database.entites.Product
+
+@Database(entities = [Product::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){
+
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase{
+            return instance ?: synchronized(this){
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "fitmeal_db"
+                ).build().also { instance = it }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/fitmeal/app/database/dao/ProductDao.kt
@@ -0,0 +1,20 @@
+package com.fitmeal.app.database.dao
+
+import kotlin.collections.List
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.fitmeal.app.database.entites.Product
+
+@Dao
+interface ProductDao {
+    @Insert
+    suspend fun insert(product: Product)
+
+    @Query("SELECT * FROM Product")
+    suspend fun getAll(): List<Product>
+
+    @Delete
+    suspend fun delete(product: Product)
+}
